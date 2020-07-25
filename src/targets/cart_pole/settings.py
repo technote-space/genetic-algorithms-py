9125569d23e01917969ff4974663fafddeda305b
Technote <technote.space@gmail.com>
2020-07-25 15:12:30 +0900

    feat: add cart pole (#1)

---
+++ src/targets/cart_pole/settings.py
@@ -0,0 +1,25 @@
+from target import AbstractSettings
+
+
+class Settings(AbstractSettings):
+    """
+    Description:
+    ------------
+    設定
+    """
+
+    @property
+    def name(self):
+        return 'Cart Pole'
+
+    @property
+    def action_limit(self):
+        return 200
+
+    @property
+    def perception_number(self):
+        return 4
+
+    @property
+    def action_number(self):
+        return 2
