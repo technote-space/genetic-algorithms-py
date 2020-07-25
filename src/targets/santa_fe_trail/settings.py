d390d20dbf600464d290db05929c4319c35f3acd
Technote <technote.space@gmail.com>
2020-07-25 15:15:03 +0900

    chore: step limit setting

--- src/targets/santa_fe_trail/settings.py
+++ src/targets/santa_fe_trail/settings.py
@@ -18,10 +18,6 @@ class Settings(AbstractSettings):
         return Helper.get_energy()
 
     @property
-    def step_limit(self):
-        return Helper.get_step_limit()
-
-    @property
     def perception_number(self):
         return 1
 
