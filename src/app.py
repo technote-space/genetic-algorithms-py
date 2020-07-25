861445a9f76f6e23dac869848cde8066c487f4bc
Technote <technote.space@gmail.com>
2020-07-25 15:13:02 +0900

    chore: specify target by args

--- src/app.py
+++ src/app.py
@@ -1,11 +1,27 @@
+import argparse
 from runner import Runner
-from targets import SantaFeTrail
+from targets import SantaFeTrail, CartPole
 from grape import Algorithm
 
 
+def get_targets():
+    return {
+        'santa-fe-trail': SantaFeTrail,
+        'cart-pole': CartPole,
+    }
+
+
+def get_choices():
+    return get_targets().keys()
+
+
 def main():
+    parser = argparse.ArgumentParser(description='Genetic Algorithm for Python')
+    parser.add_argument('target', help='target', choices=get_choices())
+    args = parser.parse_args()
+
     Runner(
-        Algorithm(SantaFeTrail(), lambda algorithm: algorithm.draw()),
+        Algorithm(get_targets()[args.target]()),
         1
     )
 
