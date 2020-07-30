93038ca596ae1aa456a11cd1df0de18cbadba5b6
Technote <technote.space@gmail.com>
2020-07-30 15:43:24 +0900

    chore: simplify

--- src/targets/breakout/ga_settings.py
+++ src/targets/breakout/ga_settings.py
@@ -7,3 +7,19 @@ class GaSettings(AbstractAtariGaSettings):
     ------------
     GAの設定
     """
+
+    @property
+    def test_number(self) -> int:
+        return 3
+
+    @property
+    def population_size(self) -> int:
+        return 50
+
+    @property
+    def island_number(self) -> int:
+        return 5
+
+    @property
+    def cultural_island_rate(self) -> float:
+        return 0
